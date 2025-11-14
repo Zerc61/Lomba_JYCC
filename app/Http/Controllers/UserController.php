3f4ce2998a16c7e3347ccd9422c91837667615a2
Zerc61 <sampbernad@gmail.com>
2025-11-14 14:38:54 +0700

    Update API-user/LOGIN

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
     public function index()
     {
         $user = User::latest()->paginate(10);
+
         return [
             "status" => 1,
             "data" => $user
@@ -26,17 +27,34 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'username' => 'required|max:20',
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
         ]);
 
         $user = User::create([
+            'username' => $request->username,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            "status" => 1,
+            "data" => $user,
+            "token" => $token,
+            "token_type" => "bearer"
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
         return [
             "status" => 1,
             "data" => $user
@@ -44,26 +62,44 @@ class UserController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+           'username' => 'required|max:20',
+           'name' =>'required',
+           'email' => 'required|email|unique|users,emaii,' .$user->id,
+           'password' => 'nullable|min:6',
+        ]);
+
+        $user->username = $request->username;
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if($request->password) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user -> save();
+
+        return [
+            "status" => 1,
+            "data" => $user,
+            "msg" => "user update succesfully"
+        ];
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user -> delete();
+        return [
+            "status" => 1,
+            "data" => $user,
+            "msg" => "user delete success"
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API berjalan']);
 });
+
+//ALTER TABLE users AUTO_INCREMENT = 1
